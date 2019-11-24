81569fe94c7c7cd828a3bdf41a908c5f5a3b38e3
Matthias Evering <36561948+ewingson@users.noreply.github.com>
2019-11-24 11:38:21 +0100

    added docstring
    documentation

--- getch.py
+++ getch.py
@@ -1,5 +1,7 @@
 #os-independent(windows and unixoid) read character in a loop without return and output ascii-value break on #
 class _Getch(object):
+	"""this will go into the docstring. the programm reads a character from stdin (which usally is the keyboard),
+	outputs its ascii-value and the loop is terminated by the hashtag. it will work on windows- and unixoid systems.""" 
     def __init__(self):
         try:
             self.impl = _GetchWindows()
