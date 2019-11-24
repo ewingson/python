4629f40492aad016010eba468d9f9604758fa777
Matthias Evering <36561948+ewingson@users.noreply.github.com>
2019-11-24 11:55:38 +0100

    indentation whitespaces
    f

--- getch.py
+++ getch.py
@@ -1,15 +1,15 @@
 #os-independent(windows and unixoid) read character in a loop without return and output ascii-value break on #
 class _Getch(object):
-	"""this will go into the docstring. the programm reads a character from stdin (which usally is the keyboard),
-	outputs its ascii-value and the loop is terminated by the hashtag. it will work on windows- and unixoid systems.""" 
-	def __init__(self):
-		try:
-			self.impl = _GetchWindows()
-		except ImportError:
-			self.impl = _GetchUnix()
+    """this will go into the docstring. the programm reads a character from stdin (which usally is the keyboard),
+    outputs its ascii-value and the loop is terminated by the hashtag. it will work on windows- and unixoid systems.""" 
+    def __init__(self):
+        try:
+            self.impl = _GetchWindows()
+        except ImportError:
+            self.impl = _GetchUnix()
 
-	def __call__(self): 
-		return self.impl()
+    def __call__(self): 
+        return self.impl()
 
 class _GetchUnix(object):
     def __init__(self):
@@ -35,14 +35,14 @@ class _GetchWindows(object):
         return msvcrt.getch()
 
 def main():
-	foo = _Getch()
-	while True:
-		char = foo.impl()
-		wert = ord(char)
-		print (char + " _ " + str(wert))
-		if (char == "#"):
-			break
-	print ("programmende")
+    foo = _Getch()
+    while True:
+        char = foo.impl()
+        wert = ord(char)
+        print (char + " _ " + str(wert))
+        if (char == "#"):
+            break
+    print ("programmende")
 		
 if __name__ == "__main__":
     main()
